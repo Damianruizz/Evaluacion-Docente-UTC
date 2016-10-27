edaddcecf9f4360f1c9994f31775dcfb608bed96
damianruizz <damian.ruiz@outlook.es>
2016-10-27 16:20:47 -0500

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -54,10 +61,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'database'  => 'bd_universidad_2',
-            'username'  => 'root',
-            'password'  => 'root',
+            'host'      => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
